69c17be0fbe5911def0d970e2c021acff0306ca5
Stuart Nelson <stuartnelson3@gmail.com>
2015-04-14 21:59:41 +0200

    lots of stuff

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
 
 get '/' do
   erb :index
+end
+
+get '/authors' do
+  erb :authors
+end
+
+get '/bibliography' do
+  erb :bibliography
 end
